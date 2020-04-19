271d103699347a5d8696dff1f7d8311d98e89e02
mihir20 <mihir20121997@gmail.com>
2020-04-20 00:58:18 +0530

    category filter added

--- app/src/main/java/com/example/covid19/ResourcesActivity.java
+++ app/src/main/java/com/example/covid19/ResourcesActivity.java
@@ -30,7 +30,7 @@ public class ResourcesActivity extends AppCompatActivity {
     RecyclerView resRV;
     ArrayList<Resource> resources,currList;
     Spinner stateSpnr, catagorySpnr;
-    ArrayList<String>states;
+    ArrayList<String>states,categories;
     private ResourcesAdapter mAdapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,21 +41,55 @@ public class ResourcesActivity extends AppCompatActivity {
         stateSpnr.setVisibility(View.INVISIBLE);
         catagorySpnr.setVisibility(View.INVISIBLE);
         createList();
+        catagorySpnr.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                currList.clear();
+                if (position==0&&stateSpnr.getSelectedItemPosition()==0){
+                    currList.addAll(resources);
+                }else{
+                    for(int i=0;i<resources.size();i++) {
+                        if ((resources.get(i).getCategory().equals(categories.get(position))||position==0) &&
+                                (resources.get(i).getState().equals(states.get(stateSpnr.getSelectedItemPosition()))||
+                                        stateSpnr.getSelectedItemPosition()==0)) {
+                            currList.add(resources.get(i));
+                        }
+                    }
+                }
+
+                if(currList.isEmpty()){
+                    currList.add(new Resource("No Resource available","","",
+                            "","","",""));
+                }
+                mAdapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
         stateSpnr.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
                 currList.clear();
-                if (position==0){
+                if (position==0&&catagorySpnr.getSelectedItemPosition()==0){
                     currList.addAll(resources);
                     mAdapter.notifyDataSetChanged();
                 }else{
-                    for(int i=0;i<resources.size();i++){
-                        if(resources.get(i).getState().equals(states.get(position))){
+                    for(int i=0;i<resources.size();i++) {
+                        if ((resources.get(i).getState().equals(states.get(position))||position==0) &&
+                                (resources.get(i).getCategory().equals(categories.get(catagorySpnr.getSelectedItemPosition()))||
+                                        catagorySpnr.getSelectedItemPosition()==0)) {
                             currList.add(resources.get(i));
                         }
                     }
-                    mAdapter.notifyDataSetChanged();
                 }
+                if(currList.isEmpty()){
+                    currList.add(new Resource("No Resource available","","",
+                            "","","",""));
+                }
+                mAdapter.notifyDataSetChanged();
             }
 
             @Override
@@ -90,6 +124,7 @@ public class ResourcesActivity extends AppCompatActivity {
 
     private void setSpinners() {
         setStateFilter();
+        setCategoryFilter();
     }
 
     private void setStateFilter() {
@@ -104,6 +139,22 @@ public class ResourcesActivity extends AppCompatActivity {
         ArrayAdapter adapter1=new ArrayAdapter(getApplicationContext(),R.layout.support_simple_spinner_dropdown_item,states);
         stateSpnr.setAdapter(adapter1);
         stateSpnr.setVisibility(View.VISIBLE);
+        stateSpnr.setSelection(0);
+    }
+
+    private void setCategoryFilter() {
+        Set<String> categorySet;
+        categorySet=new HashSet<String>();
+        categories=new ArrayList<String>();
+        for(int i=0;i<resources.size();i++){
+            categorySet.add(resources.get(i).getCategory());
+        }
+        categories.add("All");
+        categories.addAll(categorySet);
+        ArrayAdapter adapter1=new ArrayAdapter(getApplicationContext(),R.layout.support_simple_spinner_dropdown_item,categories);
+        catagorySpnr.setAdapter(adapter1);
+        catagorySpnr.setVisibility(View.VISIBLE);
+        catagorySpnr.setSelection(0);
     }
 
     private void intiViews() {
