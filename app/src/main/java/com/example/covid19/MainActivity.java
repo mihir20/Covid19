a3c46f2ed8a8f6890d33612a99d22b3ede9f26cc
mihir20 <mihir20121997@gmail.com>
2020-04-19 16:06:50 +0530

    resources activity added

--- app/src/main/java/com/example/covid19/MainActivity.java
+++ app/src/main/java/com/example/covid19/MainActivity.java
@@ -2,13 +2,28 @@ package com.example.covid19;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+    private Button statsBtn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        intiViews();
+        statsBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent res_intent = new Intent(getApplicationContext(),ResourcesActivity.class);
+                startActivity(res_intent);
+            }
+        });
+    }
+
+    private void intiViews() {
+        statsBtn = findViewById(R.id.stats_btn);
     }
 }
